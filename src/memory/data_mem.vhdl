89c7edd7c6ada33f41e3af5db486e004546239e5
DylanChambo <dylan.chambo@gmail.com>
2023-09-04 13:14:45 +1200

    feat: :sparkles: add data memory component and test bench

--- src/memory/data_mem.vhdl
+++ src/memory/data_mem.vhdl
@@ -1,16 +1,36 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.types.all;
 
 entity data_mem is
+    generic (
+        ADDR_WIDTH : integer := 10
+    );
     port (
         I_CLK : in std_logic;
-        I_RST : in std_logic
+        I_ADDR : in std_logic_vector(31 downto 0);
+        I_DATA : in std_logic_vector(31 downto 0);
+        I_RD_EN, I_WR_EN : in std_logic;
+        O_DATA : out std_logic_vector(31 downto 0)
     );
 end data_mem;
 
 architecture behavioural of data_mem is
-
+    type Mem is array (0 to (2 ** ADDR_WIDTH) - 1) of std_logic_vector (31 downto 0);
+    signal L_RAM : Mem := ((others => (others => '0')));
+    signal L_RAM_ADDR : std_logic_vector(ADDR_WIDTH - 1 downto 0);
 begin
+    process (I_CLK)
+    begin
+        if (rising_edge(I_CLK)) then
+            if (I_WR_EN = '1') then
+                L_RAM(to_integer(unsigned(L_RAM_ADDR))) <= I_DATA;
+            end if;
+        end if;
+    end process;
+    L_RAM_ADDR <= I_ADDR(ADDR_WIDTH + 1 downto 2);
 
+    O_DATA <= L_RAM(to_integer(unsigned(L_RAM_ADDR))) when (I_RD_EN = '1') else
+        x"00000000";
 end architecture;
---
+++ test/test_data_mem.vhdl
@@ -0,0 +1,69 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+use IEEE.STD_LOGIC_ARITH.all;
+
+use work.Types.all;
+
+use std.textio.all;
+use std.env.finish;
+
+entity tb_data_mem is
+end tb_data_mem;
+
+architecture sim of tb_data_mem is
+    constant clk_hz : integer := 1e9;
+    constant clk_period : time := 1 sec / clk_hz;
+    signal L_CLK : std_logic := '0';
+
+    signal L_ADDR : std_logic_vector(31 downto 0) := (others => '0');
+    signal L_DATA : std_logic_vector(31 downto 0) := (others => '0');
+    signal L_RD_EN, L_WR_EN : std_logic := '0';
+    signal D_DATA : std_logic_vector(31 downto 0);
+begin
+    L_CLK <= not L_CLK after clk_period / 2;
+
+    DUT : entity work.data_mem
+        port map(L_CLK, L_ADDR, L_DATA, L_RD_EN, L_WR_EN, D_DATA);
+
+    SEQUENCER_PROC : process
+    begin
+        L_ADDR <= (others => '0');
+        L_DATA <= x"ABCDEF01";
+        L_WR_EN <= '1';
+        wait for clk_period;
+        L_WR_EN <= '0';
+        wait for clk_period;
+        L_ADDR <= x"00000004";
+        L_DATA <= x"F0F0F0F0";
+        L_WR_EN <= '1';
+        wait for clk_period;
+        L_WR_EN <= '0';
+        wait for clk_period;
+
+        L_ADDR <= x"00000000";
+        L_RD_EN <= '1';
+        wait for clk_period/10;
+
+        assert (D_DATA = x"ABCDEF01")
+        report "INCORRECT Data at ADDR = 0x0"
+            severity failure;
+
+        L_RD_EN <= '0';
+        wait for clk_period/10;
+
+        assert (D_DATA = x"00000000")
+        report "INCORRECT Data when RD_EN = '0'"
+            severity failure;
+
+        L_ADDR <= x"00000004";
+        L_RD_EN <= '1';
+        wait for clk_period/10;
+
+        assert (D_DATA = x"F0F0F0F0")
+        report "INCORRECT Data when ADDR = 0x4"
+            severity failure;
+    end process;
+
+end architecture;
