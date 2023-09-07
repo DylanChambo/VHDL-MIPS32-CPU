5b2b4465abbbcfa851232983ec0aa6b8640e4cd7
DylanChambo <dylan.chambo@gmail.com>
2023-09-07 18:12:29 +1200

    feat: :sparkles: basic test bench

--- src/control/forwarding.vhdl
+++ src/control/forwarding.vhdl
@@ -21,12 +21,14 @@ begin
     begin
         O_FORWARD_A <= "00";
         O_FORWARD_B <= "00";
+
         if (I_MEM_WB_REG_WRITE = '1' and I_EX_MEM_REG_RD /= 0) then
             if (I_EX_MEM_REG_RD = I_ID_EX_REG_RS) then -- EX Stage forwarding
                 O_FORWARD_A <= "01";
             elsif (I_MEM_WB_REG_RD = I_ID_EX_REG_RS) then -- Mem Stage forwarding
                 O_FORWARD_A <= "10";
             end if;
+
             if (I_EX_MEM_REG_RD = I_ID_EX_REG_RT) then -- EX Stage forwarding
                 O_FORWARD_B <= "01";
             elsif (I_MEM_WB_REG_RD = I_ID_EX_REG_RT) then -- Mem Stage forwarding
